265371129137ec30142235b810c997dc285a24cd
wizard supreme <smn7818@gmail.com>
2025-04-24 03:43:17 -0400

    bootstrap: menu

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,9 @@ mod routes;
 
 // Define assets
 const BOOTSTRAP_CSS: Asset = asset!("/assets/bootstrap.min.css");
-const BOOTSTRAP_ICONS_CSS: Asset = asset!("/assets/bootstrap-icons.css");
 const BOOTSTRAP_JS: Asset = asset!("/assets/bootstrap.bundle.min.js");
 const FAVICON: Asset = asset!("/assets/favicon.ico");
+// Remove the bootstrap-icons.css asset since we'll use CDN
 
 fn main() {
     wasm_logger::init(wasm_logger::Config::default());
@@ -19,7 +19,11 @@ fn main() {
 fn App() -> Element {
     rsx! {
         document::Link { rel: "stylesheet", href: BOOTSTRAP_CSS }
-        document::Link { rel: "stylesheet", href: BOOTSTRAP_ICONS_CSS }
+        // Use CDN for Bootstrap Icons
+        document::Link { 
+            rel: "stylesheet", 
+            href: "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" 
+        }
         document::Link { rel: "icon", href: FAVICON }
         document::Script { src: BOOTSTRAP_JS }
         Router::<Route> {}
